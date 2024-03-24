1d59e6894b67a874e8f4e4bfa82db6eb707463f4
Lluís Gallart <lluis.gallart@estudiants.urv.cat>
2024-03-24 17:47:14 +0100

    Chat Grupal

---
+++ recived_logs.py
@@ -0,0 +1,54 @@
+import pika
+import threading
+from queue import Queue
+
+# Cola para la comunicación entre hilos
+cola = Queue()
+
+def enviar_mensaje():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    
+    channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+    print("Chat grupal")
+    while True:
+        entrada = input()
+        if entrada.lower() == 'exit':
+            print("Saliendo del bucle.")
+            break
+        else:
+            print("Enviado correctamente")
+            message = entrada
+            channel.basic_publish(exchange='logs', routing_key='', body=message)
+    
+    connection.close()
+
+def recibir_mensaje():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    result = channel.queue_declare(queue='', exclusive=True)
+    queue_name = result.method.queue
+
+    channel.queue_bind(exchange='logs', queue=queue_name)
+
+    def callback(ch, method, properties, body):
+        print(f"[x] {body}")
+
+    channel.basic_consume(
+        queue=queue_name, on_message_callback=callback, auto_ack=True)
+    channel.start_consuming()
+    
+    connection.close()
+
+# Crear e iniciar los hilos
+thread_enviar = threading.Thread(target=enviar_mensaje)
+thread_recibir = threading.Thread(target=recibir_mensaje)
+
+thread_enviar.start()
+thread_recibir.start()
+
+# Esperar a que ambos hilos terminen
+thread_enviar.join()
+thread_recibir.join()
