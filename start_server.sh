d23256e7b5b02e05b62596831f70c488de857a90
ines-ortiz <inesortizfigueras@gmail.com>
2024-04-21 19:45:25 +0200

    readme i start_server.sh

---
+++ start_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+gnome-terminal -- bash -c "python3 insults/insultServer.py"
+
+gnome-terminal -- bash -c "redis-server"
+
+gnome-terminal -- bash -c "./start_client.sh"
+
+sudo docker run -it --rm --name rabbitmq -p 5673:5673 -p 15672:15672 rabbitmq:3.13-management
